a61acddc26787f4b6e3466b9f1985ae302121618
danielchernov <75706332+danielchernov@users.noreply.github.com>
2023-12-17 13:04:52 -0300

    Finished Prototype 1

---
+++ Source/CryptRaider/TriggerComponent.cpp
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TriggerComponent.h"
+
+UTriggerComponent::UTriggerComponent()
+{
+    // Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+    // off to improve performance if you don't need them.
+    PrimaryComponentTick.bCanEverTick = true;
+
+    // ...
+}
+
+void UTriggerComponent::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    if (Mover == nullptr)
+        return;
+
+    AActor *AcceptableActor = GetAcceptableActor();
+    if (AcceptableActor != nullptr)
+    {
+        UPrimitiveComponent *Component = Cast<UPrimitiveComponent>(AcceptableActor->GetRootComponent());
+        if (Component != nullptr)
+        {
+            Component->SetSimulatePhysics(false);
+        }
+
+        AcceptableActor->AttachToComponent(this, FAttachmentTransformRules::KeepWorldTransform);
+
+        Mover->SetShouldMove(true);
+    }
+    else
+    {
+        Mover->SetShouldMove(false);
+    }
+}
+
+AActor *UTriggerComponent::GetAcceptableActor() const
+{
+    TArray<AActor *> Actors;
+    GetOverlappingActors(Actors);
+
+    for (AActor *Actor : Actors)
+    {
+        bool HasAcceptableTag = Actor->ActorHasTag(TagName);
+        bool IsGrabbed = Actor->ActorHasTag("Grabbed");
+
+        if (HasAcceptableTag && !IsGrabbed)
+        {
+            return Actor;
+        }
+    }
+
+    return nullptr;
+}
+
+void UTriggerComponent::SetMover(UMover *NewMover)
+{
+    Mover = NewMover;
+}
